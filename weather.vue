c16da7d19918b30aa5212811e78b5e45315e4ebb
aibeam <aibeam1025@gmail.com>
2021-11-29 22:29:44 +0900

    Initial commit

---
+++ weather.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="main" :class="isDay ? 'day' : 'night'">
+    <div class="container">
+        <h1 id="title">Weather Closet</h1>
+        <form id="searchBox">
+            <input type="text" placeholder="도시를 선택하세요...">
+        </form>
+
+        <div>
+            <div icon="sunny" v-if="clearSky" data-label="Sunny">
+                <span class="sun"></span>
+            </div>
+        </div>
+    </div>
+</template>
